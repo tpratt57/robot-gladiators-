5d9aa84797fffb269d4716d87e37d40d439fe3cd
unknown <pratttrevor79@gmail.com>
2022-05-12 18:29:21 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,7 +69,60 @@ var fight = function(enemyName) {
   } // end of while loop
 }; // end of fight function
 
-// fight each enemy-robot by looping over them and fighting them one at a time
+
+//function to start a new game
+var startGame = function()
+{
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) 
+  {
+    if (playerHealth > 0) 
+    {
+      //displaying round player is in
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      //pick enemy name based off index of enemyNames array
+      var pickedEnemyName = enemyNames[i];
+      //reset enemy health at start of new game 
+      enemyHealth = 50;
+      //sending pickedEnemyName into startGame function
+      fight(pickedEnemyName);
+    }
+    else
+    {
+      window.alert("You have lost your robot battle! Game Over! ");
+      break;
+    }
+     //after the loop ends, player is either out of health or enemies to fight, so run the end game function
+    endGame();
+  };
+   
+  //function to end the entire game 
+  var endGame = function(){
+    //if player is still alive, player wins!
+    if(playerHealth > 0){
+      window.alert("Great job, you survived the game! You now have a score of " + playerMoney + "!");
+    }
+    else{
+      window.alert("You've lost your robot in battle!");
+    }
+    window.alert("The game has now ended. Let's see how you did!");
+
+    var PlayAgainConfirm = window.confirm("Would you like to play again?");
+      if(PlayAgainConfirm){
+        //restart the game
+        startGame();
+      }
+      else{
+        window.alert("Thank you for playing Robot Gladiators! Come back again soon!")
+      }
+  };
+  
+  
+
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
@@ -93,4 +146,8 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
+};
+
 }
+// fight each enemy-robot by looping over them and fighting them one at a time
+startGame(); debugger;
